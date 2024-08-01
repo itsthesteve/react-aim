424a508a3ddf9f52bb8f31c34fd3ba155ce89515
Steve <stevenegeer@gmail.com>
2024-07-31 23:29:36 -0700

    Add KeyboardSpan component
    In light of keeping some of the same functionality of the original AIM
    experience that I don't fully remember, this gets the first letter of
    the command, and checks when the alt key is pressed by itself. After that,
    it will listen for the next key press and perform the action passed in to
    the listener prop.
    
    - Other style changes and updates to the textarea to keep state in tact

--- client/src/components/ChatInput/index.tsx
+++ client/src/components/ChatInput/index.tsx
@@ -1,15 +1,17 @@
-import { FormEventHandler, useRef, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import { useMessages } from "../../context/messages/hook";
+import KeyboardSpan from "../KeyboardSpan";
 import styles from "./styles.module.css";
 
 export default function ChatInput() {
   const { sendMessage } = useMessages();
   const [message, setMessage] = useState<string>("");
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
-  const inputRef = useRef<HTMLTextAreaElement>(null);
-
-  const submitMessage: FormEventHandler = (e) => {
-    e.preventDefault();
+  const submit = () => {
+    if (!message.length) {
+      return console.warn("Nothing to submit");
+    }
 
     sendMessage({
       channel: "abc",
@@ -20,29 +22,42 @@ export default function ChatInput() {
       },
     });
 
-    if (!inputRef.current) {
-      return;
+    if (!textAreaRef.current) {
+      return; // shouldn't happen, way past first render here
     }
 
-    // Seems like there's a better way to do this
     setMessage("");
-    inputRef.current.value = "";
+    textAreaRef.current.value = "";
+    textAreaRef.current.focus();
   };
+
+  // Even though we're not using message in the callback, it's needed otherwise
+  // the message is null. Not entirely sure why.
+  const sendListener = useCallback(() => {
+    submit();
+  }, [message, submit]);
 
   return (
     <section className={styles.chatInput}>
-      <form onSubmit={(e) => submitMessage(e)} className="py-2 mx-2 flex gap-2">
+      <div className="py-2 mx-2 flex gap-2">
         <div className="field-row-stacked grow">
-          <textarea ref={inputRef} onChange={(e) => setMessage(e.target.value)} rows={5}></textarea>
+          <textarea
+            autoFocus
+            ref={textAreaRef}
+            className={styles.textarea}
+            onChange={(e) => setMessage(() => e.target.value)}
+            rows={5}></textarea>
         </div>
         <button
-          type="submit"
+          onClick={submit}
+          type="button"
           disabled={!message.length}
           className={`${styles.sendButton} rounded-md flex flex-col gap-2 w-16 items-center justify-center`}>
           <img src="/aimguy.png" width="32" height="32"></img>
-          <span className="keyboard-ctrl">Send</span>
+          {/* <span className="keyboard-ctrl">Send</span> */}
+          <KeyboardSpan listener={() => sendListener()}>Send</KeyboardSpan>
         </button>
-      </form>
+      </div>
     </section>
   );
 }
---
+++ client/src/components/KeyboardSpan/index.tsx
@@ -0,0 +1,62 @@
+import { memo, useCallback, useEffect, useState } from "react";
+import styles from "./style.module.css";
+
+interface Props {
+  children: string;
+  listener: CallableFunction;
+}
+
+function KeyboardSpan({ children, listener }: Props) {
+  // If the trap has been set and we're waiting for a command
+  const [waiting, setWaiting] = useState(false);
+
+  const keyUpListener = useCallback(
+    (e: KeyboardEvent) => {
+      if (!children) {
+        return;
+      }
+
+      // If only the alt key was tapped and not held in combination
+      if (e.code.startsWith("Alt") && !e.altKey) {
+        console.log("Waiting for command...");
+        setWaiting(true);
+
+        window.addEventListener(
+          "keydown",
+          (e) => {
+            const commandChar = children.charAt(0).toLowerCase();
+            const cleaned = e.code.toLowerCase().replace("key", "");
+
+            if (cleaned === commandChar) {
+              e.preventDefault();
+              console.log("Firing listener!");
+              setWaiting(false);
+              listener();
+
+              return false;
+            }
+          },
+          { once: true }
+        );
+      }
+    },
+    [children, listener]
+  );
+
+  useEffect(() => {
+    if (typeof children !== "string") {
+      console.warn("KeyboardSpan children must be strings");
+      return;
+    }
+
+    window.addEventListener("keyup", keyUpListener);
+
+    return () => {
+      window.removeEventListener("keyup", keyUpListener);
+    };
+  }, [listener, children, keyUpListener]);
+
+  return <span className={`${styles.keyboardCtrl} ${waiting && styles.waiting}`}>{children}</span>;
+}
+
+export default KeyboardSpan;
