65211204a1e9d46102cd6694ca2836070a94bc2f
Steve <stevenegeer@gmail.com>
2024-08-03 20:15:51 -0700

    Update msg routes to accept the room param

--- server/routes/msg.ts
+++ server/routes/msg.ts
@@ -9,27 +9,35 @@ router.use(JsonResponseMiddleware);
  * Receieves a message payload from the chat window and saves to the KV
  * store with the keys "message" and the channel ID.
  */
-router.post("/msg", async (context) => {
+router.post("/msg", async ({ request, response }) => {
+  const roomId = request.url.searchParams.get("room");
+  if (!roomId) {
+    response.status = 400;
+    response.body = { ok: false, reason: "NOROOMID" };
+    return;
+  }
+
   const db = await Deno.openKv("./data/react-chat.sqlite");
   try {
-    const body = await context.request.body.json();
+    const body = await request.body.json();
     console.log("Saving message:", body);
 
-    const result = await db.set(["message", "abc", body.data.id], body.data);
-    await db.set(["last_message_id", "abc"], body.data.id, {
+    const result = await db.set(["message", roomId, body.data.id], body.data);
+    await db.set(["last_message_id", roomId], body.data.id, {
       expireIn: 3_600_000, // one hour
     });
 
     console.log("Result saving:", result);
 
-    context.response.status = 201;
-    context.response.body = { result: "OK" };
+    response.status = 201;
+    response.body = { result: "OK" };
   } catch (e) {
     console.warn("Error saving", e);
 
-    context.response.status = 400;
-    context.response.body = { result: ":(" };
+    response.status = 400;
+    response.body = { result: ":(" };
   }
+
   db.close();
 });
 
