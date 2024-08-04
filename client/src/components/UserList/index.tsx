abdbebc962b7b892841c9a8d5400cf471bffb70a
Steve <stevenegeer@gmail.com>
2024-08-03 17:08:01 -0700

    Remove harcoded data in user list

--- client/src/components/UserList/index.tsx
+++ client/src/components/UserList/index.tsx
@@ -5,11 +5,7 @@ export default function UserList() {
     <>
       <aside className="[grid-area:users] flex flex-col">
         <div className="mt-4 grow flex flex-col">
-          <header className="pb-2 pl-1">Users</header>
-          <ul className={`list-none m-0 p-0 bg-white grow ${styles.borderWindow}`}>
-            <li className={`${styles.userEntry} p-1`}>user1234</li>
-            <li className={`${styles.userEntry} p-1`}>user5934934</li>
-          </ul>
+          <header className="pb-2 pl-1">Your rooms</header>
         </div>
       </aside>
     </>
