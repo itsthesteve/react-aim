def88995103fc4cf4a08482b60a41a0a67f41117
Steve <stevenegeer@gmail.com>
2024-08-03 17:50:19 -0700

    Add users/rooms tab in place of users list

--- client/src/components/UserList/index.tsx
+++ client/src/components/UserList/index.tsx
@@ -1,10 +1,42 @@
 export default function UserList() {
   return (
     <>
-      <aside className="[grid-area:users] flex flex-col">
-        <div className="mt-4 grow flex flex-col">
-          <header className="pb-2 pl-1">Your rooms</header>
-        </div>
+      <aside className="[grid-area:users] flex flex-col mt-3">
+        <section className="tabs h-full">
+          <menu role="tablist" aria-label="Sample Tabs">
+            <button role="tab" aria-selected="true" aria-controls="tab-A">
+              Rooms
+            </button>
+            <button role="tab" aria-controls="tab-B">
+              Users
+            </button>
+          </menu>
+          <article role="tabpanel" id="tab-A" className="h-full p-0 border-none">
+            <ul className="tree-view h-full">
+              <li>
+                <details>
+                  <summary>Global (1)</summary>
+                  <ul>
+                    <li className="font-bold">abc</li>
+                  </ul>
+                </details>
+              </li>
+              <li>
+                <details>
+                  <summary>Your rooms (1)</summary>
+                  <ul>
+                    <li>xXSlayerXx</li>
+                  </ul>
+                </details>
+              </li>
+            </ul>
+          </article>
+          <article hidden role="tabpanel" id="tab-B" className="h-full p-0">
+            <ul className="h-full p-0 list-none">
+              <li className="p-1 hover:bg-slate-200">userdata</li>
+            </ul>
+          </article>
+        </section>
       </aside>
     </>
   );
