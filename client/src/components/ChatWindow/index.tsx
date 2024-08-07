ed1c2e39a10758ac9b2dbfe7bdb1433dce4443ef
Steve <stevenegeer@gmail.com>
2024-08-07 14:47:17 -0700

    Remove fragment and unused import

--- client/src/components/ChatWindow/index.tsx
+++ client/src/components/ChatWindow/index.tsx
@@ -3,43 +3,39 @@ import ChatInput from "../../components/ChatWindow/ChatInput";
 import MessagesList from "../../components/ChatWindow/MessagesList";
 import UserList from "../../components/ChatWindow/RoomsList";
 import { useAuthContext } from "../../context/auth/hook";
-import { useMessagesContext } from "../../context/messages/hook";
 import { ChatLoaderType } from "../../routes/chat";
 import styles from "./styles.module.css";
 
 export default function ChatWindow() {
   const { logout } = useAuthContext();
-  const { loading } = useMessagesContext();
   const { room } = useLoaderData() as ChatLoaderType;
   const online = [];
 
   return (
-    <>
-      <div className={`window ${styles.windowContainer}`}>
-        <div className="title-bar">
-          <div className="title-bar-text">React Chat | XP Edition</div>
-          <div className="title-bar-controls">
-            <button aria-label="Help" onClick={() => alert("todo")}></button>
-            <button aria-label="Close" onClick={logout}></button>
-          </div>
-        </div>
-        <div
-          className={`window-body my-0 grid ${styles.content}`}
-          style={{ background: "rgb(236, 233, 216)" }}>
-          <div className={styles.chatContainer}>
-            <MessagesList />
-            <ChatInput />
-            <UserList online={online} />
-          </div>
-        </div>
-        <div className="status-bar mx-0">
-          <div className="flex">
-            <p className="status-bar-field px-2">Current channel: {room}</p>
-            {/* <p className="status-bar-field pr-2">{online?.length} member(s) online</p> */}
-            <p className="status-bar-field pr-2">CPU Usage: 14%</p>
-          </div>
+    <div className={`window ${styles.windowContainer}`}>
+      <div className="title-bar">
+        <div className="title-bar-text">React Chat | XP Edition</div>
+        <div className="title-bar-controls">
+          <button aria-label="Help" onClick={() => alert("todo")}></button>
+          <button aria-label="Close" onClick={logout}></button>
         </div>
       </div>
-    </>
+      <div
+        className={`window-body my-0 grid ${styles.content}`}
+        style={{ background: "rgb(236, 233, 216)" }}>
+        <div className={styles.chatContainer}>
+          <MessagesList />
+          <ChatInput />
+          <UserList online={online} />
+        </div>
+      </div>
+      <div className="status-bar mx-0">
+        <div className="flex">
+          <p className="status-bar-field px-2">Current channel: {room}</p>
+          {/* <p className="status-bar-field pr-2">{online?.length} member(s) online</p> */}
+          <p className="status-bar-field pr-2">CPU Usage: 14%</p>
+        </div>
+      </div>
+    </div>
   );
 }
