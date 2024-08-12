4c9df26a5ba175ecca7d1dec476f1cbc792ff51e
Steve <stevenegeer@gmail.com>
2024-08-12 14:23:20 -0700

    Add import map and task
    I'll be attempting to use a local version of the oak-rate-limit repo so I'll
    need to alias it to the local version.
    
    Add a deno task and remove implicit permissions because good.

--- server/data/index.ts
+++ server/data/index.ts
@@ -1,4 +1,4 @@
 import "jsr:@std/dotenv/load";
 
-const DENO_KV_PATH = Deno.env.get("DENO_KV_PATH") ?? "./data/react-chat.sqlite";
+const DENO_KV_PATH = Deno.env.get("DENO_KV_PATH") ?? "./server/data/react-chat.sqlite";
 export const db = await Deno.openKv(DENO_KV_PATH);
