d6ddcd713f5caa3b2b8002812c91e952753f7dce
Steve <stevenegeer@gmail.com>
2024-08-25 20:06:49 -0700

    Convert CreateRoom to use XPWindow

--- client/src/components/CreateRoomWindow/index.tsx
+++ client/src/components/CreateRoomWindow/index.tsx
@@ -1,5 +1,7 @@
 import { FormEventHandler, useState } from "react";
 import { useLoaderData, useNavigate } from "react-router-dom";
+import ReactAimHeader from "~/components/ReactAimHeader";
+import XPWindow from "~/components/XPWindow";
 import { getAuthState } from "~/store/auth";
 import { ChatRoom } from "~/types/room";
 
@@ -44,71 +46,56 @@ export default function CreateRoomWindow() {
   };
 
   return (
-    <>
-      <div className="window" style={{ maxWidth: "min-content" }}>
-        <div className="title-bar">
-          <div className="title-bar-text">Create room</div>
-          <div className="title-bar-controls">
-            <button aria-label="Close" onClick={() => alert("todo")}></button>
-          </div>
-        </div>
-        <div className="window-body grid place-items-center">
-          <header className="bg-blue-700 w-40 aspect-square text-center font-sans font-bold flex flex-col text-white justify-between p-4">
-            <img className="self-end" src="/aimguy-256.png" width="112" height="122" />
-            React Instant Messenger
-          </header>
-          <form className="w-full py-4" onSubmit={onSubmit}>
-            {!submitted ? (
-              <>
-                <label htmlFor="roomName" className="px-2 flex flex-col gap-1 items-stretch">
-                  <span>Room name</span>
-                  <input
-                    onChange={(e) => setRoomName(e.target.value)}
-                    name="roomName"
-                    placeholder={`i.e. xX${user.username}SlayerXx`}
-                  />
-                  {isErr && (
-                    <div className="py-1 text-red-700">
-                      Unable to create room. Try another name.
-                    </div>
-                  )}
-                </label>
-                <div className="p-2">
-                  <input onChange={changePublicMode} type="checkbox" id="isPublic" />
-                  <label htmlFor="isPublic">Public</label>
+    <XPWindow title="Create room">
+      <ReactAimHeader />
+      <form className="w-full py-4" onSubmit={onSubmit}>
+        {!submitted ? (
+          <>
+            <label htmlFor="roomName" className="px-2 flex flex-col gap-1 items-stretch">
+              <span>Room name</span>
+              <input
+                onChange={(e) => setRoomName(e.target.value)}
+                name="roomName"
+                placeholder={`i.e. xX${user.username}SlayerXx`}
+              />
+              {isErr && (
+                <div className="py-1 text-red-700">Unable to create room. Try another name.</div>
+              )}
+            </label>
+            <div className="p-2">
+              <input onChange={changePublicMode} type="checkbox" id="isPublic" />
+              <label htmlFor="isPublic">Public</label>
 
-                  {isPublic && (
-                    <>
-                      <div className="pt-2 break-words max-w-40">
-                        Note: Public rooms can be joined by <em>anyone</em>.
-                      </div>
-                    </>
-                  )}
-                </div>
-                <section className="mx-2 mt-4 p-">
-                  <header>Your rooms</header>
-                  <ul className="p-0 m-0 list-none bg-white mt-2 h-24 overflow-auto">
-                    {rooms.map((room) => (
-                      <li className="px-2 py-1" key={room.id}>
-                        {room.name}
-                      </li>
-                    ))}
-                  </ul>
-                </section>
-                <footer className="mt-4 text-center">
-                  <button>Create room</button>
-                </footer>
-              </>
-            ) : (
-              <>
-                <div className="text-center">
-                  <div className="font-bold pb-2">Room created!</div> Redirecting...
-                </div>
-              </>
-            )}
-          </form>
-        </div>
-      </div>
-    </>
+              {isPublic && (
+                <>
+                  <div className="pt-2 break-words max-w-40">
+                    Note: Public rooms can be joined by <em>anyone</em>.
+                  </div>
+                </>
+              )}
+            </div>
+            <section className="mx-2 mt-4 p-">
+              <header>Your rooms</header>
+              <ul className="p-0 m-0 list-none bg-white mt-2 h-24 overflow-auto">
+                {rooms.map((room) => (
+                  <li className="px-2 py-1" key={room.id}>
+                    {room.name}
+                  </li>
+                ))}
+              </ul>
+            </section>
+            <footer className="mt-4 text-center">
+              <button>Create room</button>
+            </footer>
+          </>
+        ) : (
+          <>
+            <div className="text-center">
+              <div className="font-bold pb-2">Room created!</div> Redirecting...
+            </div>
+          </>
+        )}
+      </form>
+    </XPWindow>
   );
 }
